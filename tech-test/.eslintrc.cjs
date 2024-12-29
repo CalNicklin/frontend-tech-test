bc5f2d0a9534ff65640a1e0ab9a44c9d958a24df
Cal <calpnicklin@gmail.com>
2024-12-29 22:53:22 +0000

    removed the scaffolded version of eslint (9) in favour of (8) to make the style guide play nicely.

---
+++ tech-test/.eslintrc.cjs
@@ -0,0 +1,23 @@
+const { resolve } = require('path');
+
+const appProject = resolve(__dirname, './tsconfig.app.json');
+const nodeProject = resolve(__dirname, './tsconfig.node.json');
+
+module.exports = {
+  root: true,
+  extends: [
+    require.resolve('@vercel/style-guide/eslint/node'),
+    require.resolve('@vercel/style-guide/eslint/typescript'),
+    require.resolve('@vercel/style-guide/eslint/react'),
+  ],
+  parserOptions: {
+    project: [appProject, nodeProject],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: [appProject, nodeProject],
+      },
+    },
+  },
+};
