77abdceffd5ac5553d33b66c95fb83bacf4bbe05
Cal <calpnicklin@gmail.com>
2025-01-01 11:54:32 +0000

    set up path aliases

--- tech-test/vite.config.ts
+++ tech-test/vite.config.ts
@@ -1,10 +1,16 @@
+import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // eslint-disable-next-line import/no-default-export -- required for Vite
 export default defineConfig({
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": ".",
+      '@': resolve(__dirname),
+      '@client': resolve(__dirname, 'src'),
+      '@shared': resolve(__dirname, 'shared'),
+      '@server': resolve(__dirname, 'server'),
     },
   },
 })
