53fa972bf21fdbc9229c50170de31284bcab9b61
Cal <calpnicklin@gmail.com>
2025-01-06 17:01:43 +0000

    adds pact to ci

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,18 @@
-import path from "node:path";
-import { defineConfig } from "vitest/config";
+import path from 'node:path';
+import { defineConfig } from 'vitest/config';
 
 // eslint-disable-next-line import/no-default-export -- vitest config
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname),
-      "@client": path.resolve(__dirname, "./src"),
-      "@server": path.resolve(__dirname, "./server"),
-      "@shared": path.resolve(__dirname, "./shared"),
+      '@': path.resolve(__dirname),
+      '@client': path.resolve(__dirname, './src'),
+      '@server': path.resolve(__dirname, './server'),
+      '@shared': path.resolve(__dirname, './shared'),
     },
   },
   test: {
     setupFiles: ['./vitest.setup.ts'],
+    exclude: ['**/e2e/**', '**/e2etests/**', 'node_modules/**'],
   },
 });
